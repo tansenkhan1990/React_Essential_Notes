dc49f9cbc259569f120f3c4bb2b048adcac23485
tansenkhan1990 <tansenkhan1990@gmail.com>
2022-05-29 11:55:43 +0200

    add two way bindling for a form submission

--- notes.jsx
+++ notes.jsx
@@ -1,22 +1,41 @@
 // #set state based on the previous state
+mainComponents=()=>{
 
-[handleInput, setHandleInput] = useSate(
-{inputEmail:'',
-inputName: '',
-InputAge:''
-})
+    [handleInput, setHandleInput] = useSate(
+        {inputEmail:'',
+        inputName: '',
+        InputAge:''
+        })
+        
+        handleInput=(ev)=>{
+        setHandleInput((prevState)=>{
+        return (...prevState,ev.target.value)
+        })
+        }
+        hanldeOnSubmit=(evt)=>{
+            evt.preventDefault();
+            submitData={
+                email: handleInput.inputEmail,
+                name: handleInput.inputName,
+                age:handleInput.InputAge
+            }
+            setHandleInput({
+                inputEmail:'',
+                inputName: '',
+                InputAge:''
+            })
 
-handleInput(ev)=>{
-setHandleInput((prevState)=>{
-return (...prevState,ev.target.value)
-})
+        }
+        // *** Form onSubmit should be inside of the form tag
+        <form onSubmit={hanldeOnSubmit}>
+            <input type="text" value={handleInput.inputEmail} onChange={handleInput}>Email</input>
+            <input type="text" value={handleInput.inputName} onChange={handleInput}>name</input>
+            <input type="text" value={handleInput.InputAge} onChange={handleInput}>age</input>
+        </form>
+         handleOnSubmit =(evt)=>{
+             evt.preventDefault(); // this will stop to load the page
+        
+         }
 }
 
-// *** Form onSubmit should be inside of the form tag
-<form onSubmit={hanldeOnSubmit}>
-</form>
- handleOnSubmit =(evt)=>{
-     evt.preventDefault(); // this will stop to load the page
-
- }
 
